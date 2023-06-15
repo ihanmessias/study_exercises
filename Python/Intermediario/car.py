8a19e0adb1333a417609b11b0c8b4bb05a0cbb55
mrhowaito <ihanmessias.dev@gmail.com>
2023-06-15 17:21:40 -0300

    Initial exercise car

--- Python/Intermediario/car.py
+++ Python/Intermediario/car.py
@@ -10,11 +10,30 @@
 import os
 
 # ---- Funcs ---- #
-# -> Limpa Terminal:
+# -> Clear Terminal:
 def clear_terminal():
     os.system('cls' if os.name != 'posix' else 'clear')
+# -> Menu:
+def view_menu():
+    print(' | '.join([f'[{i}] - {op}' for i, op in enumerate(menu_op)]))
+    user = input('~ ')
+    return user
+# -> View Cars:
+def view_cars():
+    for i, car in enumerate(cars.items()):
+        print(f'[{i}] {car[0]} - R${car[1]} /dia')
     
 # ---- Project ---- #
-while True:
-    user = input('')
-    
+# -> Menu Options:
+menu_op = ['Mostrar portifólio', 'Alugar um carro', 'Devolver um carro']
+# -> Cars:
+cars = {'Chevrolet Tracker':120, 'Chevrolet Onix':90,
+            'Hyundai HB20':85,'Hyundai Tucson':120,
+            'Fiat Uno':60,'Fiat Mobi':70,'Fiat Pulse':130}
+
+while True:    
+    clear_terminal()
+    print('Bem vindo á locadora de carros!\nO que deseja fazer?\n')
+    user = view_menu()
+    print(user)
+    break
