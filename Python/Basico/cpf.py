8d729c12bd45c6418d4f2ff80917031e36e34b32
mrhowaito <ihanmessias.dev@gmail.com>
2023-06-27 03:29:38 -0300

    Update: "first to second"

--- Python/Basico/cpf.py
+++ Python/Basico/cpf.py
@@ -7,9 +7,9 @@ if cpf[0] * len(cpf) == cpf:
 first_digit =  (sum([int(digit) * regressive for digit, regressive in zip(cpf[:9], range(10, 1, -1))]) * 10) % 11
 second_digit = (sum([int(digit) * regressive for digit, regressive in zip(cpf[:10], range(11, 1, -1))]) * 10) % 11
 if first_digit > 9:
-    first_digit=0
+    first_digit = 0
 elif second_digit > 9:
-    first_digit =0
+    second_digit = 0
 elif cpf[9:] == f'{first_digit}{second_digit}':
     print('CPF "{}{}{}.{}{}{}.{}{}{}-{}{}" validado.'.format(*cpf))
 else:
