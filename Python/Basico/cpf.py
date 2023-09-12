04b83266f2166863eb75e749f9f75854a75695b6
mrhowaito <labs@labs>
2023-09-12 00:40:37 -0300

    Oficial Script

--- Python/Basico/cpf.py
+++ Python/Basico/cpf.py
@@ -1,21 +1,16 @@
+# -->> *Import Libs* <<-- #
 import re
+# -->> *Input* <<-- #
 cpf = re.sub(r'[^0-9]','',input('Informe o CPF: '))
+# -->> *Options* <<-- #
 if cpf[0] * len(cpf) == cpf:
-    print('cpf é invalido, dados sequenciais.')
-    exit()
-
-first_digit =  (sum([int(digit) * regressive for digit, regressive in zip(cpf[:9], range(10, 1, -1))]) * 10) % 11
-second_digit = (sum([int(digit) * regressive for digit, regressive in zip(cpf[:10], range(11, 1, -1))]) * 10) % 11
-if first_digit > 9:
-    first_digit = 0
-elif second_digit > 9:
-    second_digit = 0
-elif cpf[9:] == f'{first_digit}{second_digit}':
-    print('CPF "{}{}{}.{}{}{}.{}{}{}-{}{}" validado.'.format(*cpf))
+    print('cpf é invalido, dados sequenciais.')    
 else:
-    print('CPF "{}{}{}.{}{}{}.{}{}{}-{}{}" invalido.'.format(*cpf))
-# result = []
-# for digit, regressive in zip(cpf, range(10, 1, -1)):
-#     print(f'{digit}*{regressive}= {int(digit) * regressive}')
-#     result.append(int(digit) * regressive)
-# print(result,'=',sum(result))
+    first_digit =  (sum([int(digit) * regressive for digit, regressive in zip(cpf[:9], range(10, 1, -1))]) * 10) % 11
+    second_digit = (sum([int(digit) * regressive for digit, regressive in zip(cpf[:10], range(11, 1, -1))]) * 10) % 11
+    first_digit, second_digit = (0 if id > 9 else id for id in (first_digit, second_digit))
+
+    if cpf[9:] == f'{first_digit}{second_digit}':
+        print('CPF "{}{}{}.{}{}{}.{}{}{}-{}{}" validado.'.format(*cpf))
+    else:
+        print('CPF "{}{}{}.{}{}{}.{}{}{}-{}{}" invalido.'.format(*cpf))
